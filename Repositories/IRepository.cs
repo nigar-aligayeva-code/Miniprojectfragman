f893b5d1e0bb78849a13c84cc924367a8afc4f70
nigar-aligayeva-code <nigarmal@code.edu.az>
2026-06-29 19:16:57 +0400

    Refactor IRepository
    .

--- Repositories/IRepository.cs
+++ Repositories/IRepository.cs
@@ -2,12 +2,11 @@
 
 namespace MiniProject2.Repositories
 {
-    // public olaraq saxlayırıq
     public interface IRepository<T> where T : class, new()
     {
         void Add(T entity);
         List<T> GetAll();
-        T? GetById(int id, bool isTracking = false); // Müəllimdəki isTracking parametrini əlavə etdik         
+        T? GetById(int id, bool isTracking = false);    
         int SaveChanges();
     }
 }
