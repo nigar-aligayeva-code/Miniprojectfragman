47f7e98339f65ce79e9c47e953e94938bfcae34f
nigar-aligayeva-code <nigarmal@code.edu.az>
2026-06-29 19:15:48 +0400

    AppDbContext
     DbSet properties and connection string.

--- DAL/AppDbContext.cs
+++ DAL/AppDbContext.cs
@@ -5,14 +5,11 @@ namespace MiniProject2.DAL
 {
     public class AppDbContext : DbContext
     {
-        // Bu hissə SQL-də cədvəllərimizin yaranmasını təmin edir
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // Kompüterinizdəki SQL Server-ə qoşulmaq üçün bağlantı mətni (ConnectionString).
-            // Server=. hissəsi sizin lokal SQL Serverinizə işarə edir.
             optionsBuilder.UseSqlServer(@"Server=DESKTOP-RSKLB3V\SQLEXPRESS;Database=MiniProject2DB;Trusted_Connection=True;TrustServerCertificate=True;");
         }
     }
